c3e68ac23b01e81ec14ac9024fd6c3fb0fc6cb21
RWC <39197176+readingwritingcode@users.noreply.github.com>
2020-11-09 14:36:12 -0500

    Create read.py
    here we will build Read User Data Stepts

---
+++ read.py
@@ -0,0 +1,9 @@
+  ''' basic things that we should think about from now'''
+  
+  # read unstructure or structure user data
+  
+  # make user query
+  
+  # store user query
+  
+  # validate user query with other user queryes!
